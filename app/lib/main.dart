6caf9b380ceb9f47e80655cac69048be70290274
Antonino Praxedes <antonino.praxedes@gmail.com>
2024-11-04 23:44:26 -0300

    feat: adicionando novo layout inicial

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,28 +2,62 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Frases do dia',
-      home: Scaffold(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
-          title: Text('Instagram'),
+          title: Text("Frases do dia", style: TextStyle(color: Colors.white)),
           backgroundColor: Colors.green,
         ),
-        body: Padding(
-          padding: EdgeInsets.all(16),
-          child: Text('Conteúdo principal'),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          color: Colors.lightGreen,
-          child: Padding(
+        body: Center(
+          child: Container(
             padding: EdgeInsets.all(16),
-            child: Row(
+            decoration: BoxDecoration(
+              border: Border.all(width: 3, color: Colors.amber),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text('Texto 1'),
-                Text('Texto 2'),
+                Image.asset("images/logo.png"),
+                Text(
+                  "Clique abaixo para gerar uma frase!",
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.black),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green, // Cor de fundo do botão
+                    foregroundColor: Colors.white, // Cor do texto
+                    textStyle: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onPressed: () {
+                    // Coloque aqui a ação do botão
+                  },
+                  child: Text("Nova Frase"),
+                )
               ],
             ),
           ),
-        ),
-      )));
+        ));
+  }
 }
